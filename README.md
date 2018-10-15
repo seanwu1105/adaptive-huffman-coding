1032831ff5668ac6b5696a987ab3fb0069cd45e6
Sean Wu <seanwu1105@gmail.com>
2018-10-15 23:42:19 +0800

    add requirements and license

---
+++ README.md
@@ -0,0 +1,41 @@
+# Adaptive Huffman Coding Compression
+
+## Testing Instances
+
+### `Lena.raw`
+
+### `Baboon.raw`
+
+## Algorithm
+
+### Encode Procedure
+
+### Decode Procedure
+
+### Update Procedure
+
+## Dependencies
+
+* [bitarray](https://github.com/ilanschnell/bitarray)
+
+``` bash
+pip3 install bitarray
+```
+
+* [NumPy](http://www.numpy.org/)
+
+``` bash
+pip3 install numpy
+```
+
+* [Matplotlib](https://matplotlib.org/)
+
+``` bash
+pip3 install matplotlib
+```
+
+* [progress](https://github.com/verigak/progress/)
+
+``` bash
+pip3 install progress
+```
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    compress('Lena.raw', 'compressed', alphabet_range=(0, 255))
+    compress('tests/images/Lena.raw', 'compressed', alphabet_range=(0, 255))
     extract('compressed', 'extract.raw', alphabet_range=(0, 255))
-    show_raw_img('extract.raw', size=(512, 512))
+    show_raw_img('tests/images/Lena.raw', 'extract.raw', size=(512, 512))
 
 
 def compress(in_filename, out_filename, alphabet_range):
@@ -41,14 +41,18 @@ def extract(in_filename, out_filename, alphabet_range):
                                      os.path.getsize(out_filename))
 
 
-def show_raw_img(img_filename, size):
+def show_raw_img(original_filename, extracted_filename, size):
     from matplotlib import pyplot as plt
     import numpy as np
 
-    with open(img_filename, 'rb') as img_file:
-        img = np.fromfile(img_file, dtype=np.uint8)
-    img.shape = size
-    plt.imshow(img, cmap='gray')
+    with open(original_filename, 'rb') as img_file:
+        original_img = np.fromfile(img_file, dtype=np.uint8)
+    with open(extracted_filename, 'rb') as img_file:
+        extracted_img = np.fromfile(img_file, dtype=np.uint8)
+    original_img.shape = extracted_img.shape = size
+    _, axarr = plt.subplots(1, 2)
+    axarr[0].imshow(original_img, cmap='gray')
+    axarr[1].imshow(extracted_img, cmap='gray')
     plt.show()
 
 
