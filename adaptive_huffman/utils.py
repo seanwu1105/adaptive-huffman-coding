20d5e6bac48fb8d60961fbea011f9788b5341352
Sean Wu <seanwu1105@gmail.com>
2018-12-02 17:21:48 +0800

    change bitarray to list, but the result performance is really bad

--- adaptive_huffman/utils.py
+++ adaptive_huffman/utils.py
@@ -1,5 +1,19 @@
+import itertools
+
 def encode_dpcm(seq):
     return (
         (item - seq[idx - 1]) & 0xff if idx else item
         for idx, item in enumerate(seq)
     )
+
+def decode_dpcm(seq):
+    return itertools.accumulate(seq, lambda x, y: (x + y) & 0xff)
+
+def bin_str2bool_list(s):
+    return [False if c == '0' else True for c in s]
+
+def bool_list2bin_str(l):
+    return ''.join('1' if i else '0' for i in l)
+
+def bool_list2int(l):
+    return int(bool_list2bin_str(l), 2)
